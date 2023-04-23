7e85f48ae45f11f9e0704e603aedfff094762610
chenxingyu <chenxingyusean@163.com>
2023-04-23 18:47:27 +0800

    update python usage

--- handavatar/scripts/run_hand.sh
+++ handavatar/scripts/run_hand.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 /mnt/user/chenxingyu/.conda/envs/pt111_mnt/bin/python handavatar/run_interhand.py \
+CUDA_VISIBLE_DEVICES=0 python handavatar/run_interhand.py \
     --cfg 'handavatar/configs/interhand/test_cap0.yaml'
--- handavatar/scripts/train_hand.sh
+++ handavatar/scripts/train_hand.sh
@@ -1,3 +1,3 @@
 
-CUDA_VISIBLE_DEVICES=0 NVIDIA_TF32_OVERRIDE=0 /mnt/user/chenxingyu/.conda/envs/pt111_mnt/bin/python handavatar/train.py \
+CUDA_VISIBLE_DEVICES=0 NVIDIA_TF32_OVERRIDE=0 python handavatar/train.py \
     --cfg 'handavatar/configs/interhand/test_cap0.yaml'
